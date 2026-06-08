34ff69ced66e8cc6878a4c126050bc8cb3c254ac
Sun Bing <762312820@qq.com>
2026-06-08 10:29:21 +0800

    调整文件位置

--- SqlStatement/query.py
+++ SqlStatement/query.py
@@ -1,7 +1,7 @@
 import psycopg
 from psycopg.rows import dict_row
 
-def exe_sql(sql_statement:str,args_tuple:tuple):
+def exe_sql(*,sql_statement:str,args_tuple:tuple):
     conn = psycopg.connect(
         "postgresql://bing@localhost:5432/first_rag",
         row_factory=dict_row,
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         """
     username = 'monkey'
 
-    res = exe_sql(sql,(username,))
+    res = exe_sql(sql_statement=sql, args_tuple=(username,))
     print(res[0]['username'])
     print(res[0]['password_hash'])
 
